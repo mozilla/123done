1ce84bcef5afc0113a496d3dabe0128d093258a5
Sean McArthur <sean.monstar@gmail.com>
2014-04-21 17:22:59 -0700

    switch to gryphon auth

--- oauth.js
+++ oauth.js
@@ -1,6 +1,8 @@
 var config = require('./config.json'),
     crypto = require('crypto'),
-    request = require('request')
+    gryphon = require('gryphon'),
+    request = require('request'),
+    url = require('url')
     ;
 
 // oauth flows are stored in memory
@@ -31,37 +33,38 @@ module.exports = function(app, db) {
     var code = req.query.code;
 
     if (code && state && state in oauthFlows) {
-      req.session.code = code;
       delete oauthFlows[state];
+      var keys = gryphon.keys();
 
       request.post({
-        uri: config.oauth_uri + '/token',
+        uri: config.oauth_uri + '/pubkey',
         json: {
           code: code,
+          pubkey: keys.pk.toString('hex'),
           client_id: config.client_id,
           client_secret: config.client_secret
         }
       }, function(err, r, body) {
-        if (err) res.send(r.status, err);
+        if (err || r.status >= 400) return res.send(r.status, err || body);
 
         console.log(err, res, body);
         req.session.scopes = body.scopes;
         req.session.token_type = body.token_type;
-        var token = req.session.token = body.access_token;
 
-        // store the bearer token
-        db.set(code, body.access_token);
+        // store the keys
+        db.set(keys.pk, keys.sk);
 
-        request.post({
-          uri: config.profile_uri + '/email',
-          headers: {
-            Authorization: 'Bearer ' + token
-          }
-        }, function (err, r, body) {
+        var opts = url.parse(config.profile_uri + '/email');
+        opts.method = 'POST';
+        opts.headers = {
+          authorization: gryphon.header(opts, keys)
+        };
+        request(opts, function (err, r, body) {
           console.log(err, r, body);
+          if (err || r.status >= 400) return res.send(r.status, err || body);
+          req.session.email = body.email;
+          res.redirect('/');
         });
-
-        res.send(200);
       });
     } else {
       res.send(400);
